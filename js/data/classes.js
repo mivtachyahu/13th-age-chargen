2ff4f0e7edda4e43f6ca674b985eee56d9185e23
Matthew Brown <matthew@freezeproshop.com>
2017-12-04 16:31:53 +0000

    minor changes

--- js/data/classes.js
+++ js/data/classes.js
@@ -2,8 +2,14 @@ define(function(require) {
   return _.chain({
     Barbarian:  [7,12,11,10,10,['str','con']],
     Bard:       [7,12,10,11, 8,['dex','cha']],
+    Chaos Mage: [8,10,10,11, 8,['int','cha']],
     Cleric:     [7,14,11,11, 8,['str','wis']],
+    Commander:  [7,,['str','cha']],
+    Druid:      [6,10,11,11, 8,['str','dex','wis']],
     Fighter:    [8,15,10,10,10,['str','con']],
+    Monk:       [7,11,11,11, 8,['str','dex','wis']]
+    Necromancer:[6,10,10,11, 8,['int','cha']]
+    Occultist:  [6,11,10,11, 8,['int','wis']]
     Paladin:    [8,16,10,12,10,['str','cha']],
     Ranger:     [7,14,11,10, 8,['str','dex','wis']],
     Rogue:      [6,12,12,10, 8,['dex','cha']],
